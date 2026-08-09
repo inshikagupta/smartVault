d5a9701b84bf1f2055eeddd06ea97fa7061d9117
Inshika Gupta <inshikagupta11@gmail.com>
2026-08-09 16:45:23 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 // CORS
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [process.env.FRONTEND_URL],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
