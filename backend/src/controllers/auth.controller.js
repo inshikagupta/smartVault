ee102dd73cc0adf3e3e9d37699c6c0658d24eaa4
Inshika Gupta <inshikagupta11@gmail.com>
2026-08-09 17:00:53 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -6,7 +6,7 @@ const sendEmail = require("../utils/sendEmail");
 
 const COOKIE_OPTS = {
   httpOnly: true,
-  sameSite: "lax",
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   secure: process.env.NODE_ENV === "production",
 };
